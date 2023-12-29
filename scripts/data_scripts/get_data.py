bb12e0430e4c77d645b0eb5479f79b4a8bb035eb
qucals <qucals@gmail.com>
2023-12-30 02:01:18 +0500

    Added new raw data and new data preprocessing script

--- scripts/data_scripts/get_data.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-from catboost.datasets import titanic
-
-train, test = titanic()
-
-train.to_csv("/home/data-srv/ml_pr1/data/raw/train.csv", columns=train.columns)
-
-test.to_csv("/home/data-srv/ml_pr1/data/raw/test.csv", columns=test.columns)
